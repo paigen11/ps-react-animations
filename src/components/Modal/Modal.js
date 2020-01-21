0f500a911c58b5fecc787bbccdb4b0bd9b1f0521
Paige Niedringhaus <paigen11@gmail.com>
2020-01-21 15:11:30 -0500

    last of the animations

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 import './Modal.css';
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
 const modal = props => {
-  const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
   return (
-    <div className={cssClasses.join(' ')}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      classNames={{
+        enter: '',
+        enterActive: 'ModalOpen',
+        exit: '',
+        exitActive: 'ModalClosed',
+      }}
+      mountOnEnter
+      unmountOnExit
+    >
+      {state => {
+        const cssClasses = [
+          'Modal',
+          state === 'entering'
+            ? 'ModalOpen'
+            : state === 'exiting'
+            ? 'ModalClosed'
+            : null,
+        ];
+        return (
+          <div className={cssClasses.join(' ')}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </CSSTransition>
   );
 };
 
